3d6586f7a01794299df13ad20fc9c87b52bd38ca
Davut-Orazgylyjov-hello-world <88382528+Davut-Orazgylyjov-hello-world@users.noreply.github.com>
2023-06-05 11:21:32 +0300

    first lessons

---
+++ C# Уроки. От новичка к профессионалу/Урок №1/Education/Education/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Education
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет Мир!");
+        }
+    }
+}
