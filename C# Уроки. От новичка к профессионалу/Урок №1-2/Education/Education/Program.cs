4218037151b5d115e258c7b96a0cc8a1ab07fe51
Davut-Orazgylyjov-hello-world <davutorazgylyjov@gmail.com>
2023-06-09 14:40:05 +0300

    added

---
+++ C# Уроки. От новичка к профессионалу/Урок №1-2/Education/Education/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Lessons of education
+
+/*Test Comments*/
+
+namespace Education
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Привет Мир!");
+        }
+    }
+}
