be6a711af9e47b5242d494dc5e4dfeec4e116fd3
Davut-Orazgylyjov-hello-world <88382528+Davut-Orazgylyjov-hello-world@users.noreply.github.com>
2023-06-11 19:54:42 +0300

    5-6

---
+++ C# Уроки. От новичка к профессионалу/Урок №5-6/Education/Education/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * ReadLine
+ */
+
+using System;
+using System.Globalization;
+
+namespace Education
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            //program.SayHelloToUser();
+            
+            Console.WriteLine(program.GetConsoleDouble() * 3);
+        }
+
+        private void SayHelloToUser()
+        {
+            var userName = Console.ReadLine();
+            
+            Console.WriteLine($"Hello {userName}!!!");
+        }
+
+        private int GetConsoleInt()
+        {
+            string str = Console.ReadLine();
+
+            return Convert.ToInt32(str);
+        }
+
+        private double GetConsoleDouble()
+        {
+            string str = Console.ReadLine();
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ",",
+            };
+            
+            return Convert.ToDouble(str, numberFormatInfo);
+        }
+    }
+}
