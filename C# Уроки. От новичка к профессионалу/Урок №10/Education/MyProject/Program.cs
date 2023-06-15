24755d08dedf0e66016b32ddc3e21a1bf47a6928
Davut-Orazgylyjov-hello-world <88382528+Davut-Orazgylyjov-hello-world@users.noreply.github.com>
2023-06-15 21:43:41 +0300

    9-10 lessons

---
+++ C# Уроки. От новичка к профессионалу/Урок №10/Education/MyProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyProject
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world From MyProject");
+            Console.ReadLine();
+        }
+    }
+}
