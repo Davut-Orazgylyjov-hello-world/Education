a28cd879003fecc3a69570c6de5ce284c25bd0f1
Davut-Orazgylyjov-hello-world <88382528+Davut-Orazgylyjov-hello-world@users.noreply.github.com>
2023-06-30 00:37:40 +0300

    16-19 lessons

---
+++ C# Уроки. От новичка к профессионалу/Урок № 16 Домашняя задание #4/Education/Education/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Education
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальное значение");
+            int startValue = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.WriteLine("Введите последнее значение");
+            int endValue = int.Parse(Console.ReadLine() ?? string.Empty);
+            uint even = 0, odd = 0;
+
+            int evenSum = 0, oddSum = 0;
+            while (startValue <= endValue)
+            {
+                if (startValue % 2 == 0)
+                {
+                    even++;
+                    evenSum += startValue;
+                }
+                else
+                {
+                    odd++;
+                    oddSum += startValue;
+                }
+                
+                startValue++;
+            }
+
+            Console.WriteLine($"Четных: {even} Нечетных: {odd}");
+            Console.WriteLine($"Сумма Четных: {evenSum} Сумма Нечетных: {oddSum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Уроки. От новичка к профессионалу/Урок № 18/Education/Education/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Education
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // int i = 0;
+            //
+            // for ( new Program().Function(), new Program().Function(); //дествие которое выполняется перед началом цикла
+            //     i < 5 && i > -1; //проверка, можно ли выполнить цикл
+            //     i++, new Program().Function()) //действие которое выполняется после каждого цикла
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // for (;;)
+            // {
+            //     Console.WriteLine("for is working");
+            //     System.Threading.Thread.Sleep(300);
+            //    // break;
+            // }
+
+            // int i = int.Parse(Console.ReadLine());
+            // for (; i >= 0; i--)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            for (int i = 0, j = 10;i <10 ;i++, j--)
+            {
+                Console.WriteLine("I:"+i);
+                Console.WriteLine("J:"+j);
+            }
+        }
+
+        public void Function()
+        {
+            Console.WriteLine("Function Activated");
+        }
+    }
+}
